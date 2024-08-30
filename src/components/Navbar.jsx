32e50ed5b48bbc68852c90306ea583fc07451f0e
MartinXCVI <mdzg96@gmail.com>
2024-08-30 11:28:01 -0300

    added react icons and basic responsive navbar items display

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,46 @@
+import { useState } from 'react';
+
 import { Link } from 'react-router-dom'
+
+import { IoMenu } from "react-icons/io5";
+import { IoIosCloseCircleOutline } from "react-icons/io";
 
 
 const Navbar = () => {
+
+  const [menuClass, setMenuClass] = useState('hidden-menu')
+
+  function displayNavMenu() {
+    setMenuClass('')
+  }
+
+  function hideNavMenu() {
+    setMenuClass('hidden-menu')
+  }
+
   return (
     <nav className="navbar">
-      <ul className="nav-list">
+      <div className="menu-div" onClick={displayNavMenu}>
+        <IoMenu className='menu-icon' />
+      </div>
+      <ul className={`nav-list ${menuClass}`}>
+        <div className="close-div" onClick={hideNavMenu}>
+          <IoIosCloseCircleOutline className='close-icon' />
+        </div>
         <li className="nav-item">
-          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/" className="nav-link" onClick={hideNavMenu}>Home</Link>
         </li>
         <li className="nav-item">
-          <Link to="/about" className="nav-link">About</Link>
+          <Link to="/about" className="nav-link" onClick={hideNavMenu}>About</Link>
         </li>
         <li className="nav-item">
-          <Link to="/services" className="nav-link">Services</Link>
+          <Link to="/services" className="nav-link" onClick={hideNavMenu}>Services</Link>
         </li>
         <li className="nav-item">
-          <Link to="/faq" className="nav-link">FAQ</Link>
+          <Link to="/faq" className="nav-link" onClick={hideNavMenu}>FAQ</Link>
         </li>
         <li className="nav-item">
-          <Link to="/contact" className="nav-link">Contact</Link>
+          <Link to="/contact" className="nav-link" onClick={hideNavMenu}>Contact</Link>
         </li>
       </ul>
     </nav>    
