8ff823d4d885236571910794f1f7db26ed868f2d
MartinXCVI <mdzg96@gmail.com>
2024-09-04 17:22:13 -0300

    redesign of landing section

--- src/pages/Home/Landing.jsx
+++ src/pages/Home/Landing.jsx
@@ -7,13 +7,17 @@ const Landing = () => {
         <section className="landing-section-left">
           <h1 className="landing-title">Welcome to <br/>Our Website</h1>
           <Link to="/services" className="landing-button">More Info</Link>
-        </section>
-        <section className="landing-section-right">
-          <figure className="landing-section-figure">
-            <img src="/icons/home-landing.png" alt="" />
-          </figure>
           <h2 className="landing-text">Your healing journey starts here</h2>
           <h2 className="landing-text">Your new beginning awaits</h2>
+        </section>
+        <section className="landing-section-right">
+          <article className="blockquote-wrapper">
+            <blockquote className="landing-blockquote">
+              <p className="landing-quote"><q>To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.</q></p>
+              <p className="landing-quote-author">—Ralph Waldo Emerson</p>
+            </blockquote>
+          </article>
+
         </section>
       </section>
     </>
